9ff4f1ba816b9ffef3d514fea912155f5b5606d3
yuvalBerghaus <yuvalBerghaus>
2023-11-03 18:01:55 -0500

    added player and movement

--- 3DFPS/Assets/Scripts/Controller/PlayerController.cs
+++ 3DFPS/Assets/Scripts/Controller/PlayerController.cs
@@ -7,8 +7,21 @@ using UnityEngine;
 /// </summary>
 public class PlayerController : MonoBehaviour
 {
+    [Header("Settings")]
+    [Tooltip("The speed at which the player moves")]
+    public float moveSpeed = 2f;
+    [Tooltip("The speed at which the player looks")]
+    public float lookspeed = 60f;
+    [Tooltip("The power of which the player jumps")]
+    public float jumpPower = 8f;
+    [Tooltip("The gravity of the world")]
+    public float gravity = 9.81f;
+    [Header("Required References")]
     public Shooter playerShooter;
-
+    [Tooltip("The player shooter script that fires projectiles")]
+    // The character controller component on the player
+    private CharacterController controller;
+    private InputManager inputManager;
     /// <summary>
     /// Description:
     /// Standard Unity function called once before the first Update call
@@ -19,9 +32,25 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void Start()
     {
-
+        SetUpCharacterController();
+        SetUpInputManager();
     }
 
+
+
+    private void SetUpCharacterController()
+    {
+        controller = GetComponent<CharacterController>();
+        if (controller == null)
+        {
+            Debug.LogError("The player controller script doesnt have a character controller on the same game object!");
+        }
+    }
+
+    void SetUpInputManager()
+    {
+        inputManager = InputManager.instance;
+    }
     /// <summary>
     /// Description:
     /// Standard Unity function called once every frame
@@ -32,6 +61,30 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void Update()
     {
+        ProcessMovement();
+    }
+    Vector3 moveDirection;
+    void ProcessMovement()
+    {
+        // Get the input from the input manager
+        float leftRightInput = inputManager.horizontalMoveAxis;
+        float forwardBackwardInput = inputManager.verticalMoveAxis;
+        bool jumpPressed = inputManager.jumpPressed; 
+        // Handle the control of the player while it is on the ground
+        if(controller.isGrounded)
+        {
+            // Set the movement direction to be the recieved input, set y to 0 since we are on the ground
+            moveDirection = new Vector3(leftRightInput, 0, forwardBackwardInput);
+            // Set the move direction in relation to the transform
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * moveSpeed;
+            if(jumpPressed)
+            {
+                moveDirection.y = jumpPower;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
 
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
